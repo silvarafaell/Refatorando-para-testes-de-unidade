4709da36d93f7207e5879b5043fe91211d47e785
Francisco Rafael <francisco_rafael@hotmail.com.br>
2021-03-25 19:37:12 -0300

    Finalizado testes de entidades

--- Store/Store.Tests/Entities/OrderTests.cs
+++ Store/Store.Tests/Entities/OrderTests.cs
@@ -17,6 +17,7 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_valido_ele_deve_gerar_um_numero_com_8_caracteres()
         {
+            //primeiro teste
             var order = new Order(_customer, 0, null);
             Assert.AreEqual(8, order.Number.Length);
         }
@@ -25,7 +26,7 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_seu_status_deve_ser_aguardando_pagamento()
         {
-            //status do pedido 
+            //segundo teste, status do pedido 
             var order = new Order(_customer, 0, null);
             Assert.AreEqual(order.Status, EOrderStatus.WaitingPayment);
         }
@@ -34,6 +35,7 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_pagamento_do_pedido_seu_status_deve_ser_aguardando_entrega()
         {
+            //terceiro teste, foi pago e esta aguardando entrega
             var order = new Order(_customer, 0, null);
             order.AddItem(_product, 1); // Total deve ser 10
             order.Pay(10);
@@ -44,6 +46,7 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_pedido_cancelado_seu_status_deve_ser_cancelado()
         {
+            //quarto teste, cancelamento do pedido
             var order = new Order(_customer, 0, null);
             order.Cancel();
             Assert.AreEqual(order.Status, EOrderStatus.Canceled);
@@ -53,6 +56,7 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_novo_item_sem_produto_o_mesmo_nao_deve_ser_adicionado()
         {
+            //quinto teste, adição de item
             var order = new Order(_customer, 0, null);
             order.AddItem(null, 10);
             Assert.AreEqual(order.Items.Count, 0);
@@ -62,6 +66,7 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_novo_item_com_quantidade_zero_ou_menor_o_mesmo_nao_deve_ser_adicionado()
         {
+            //sexto teste,  adicionando item invalido com quantidade zero
             var order = new Order(_customer, 0, null);
             order.AddItem(_product, 0);
             Assert.AreEqual(order.Items.Count, 0);
@@ -71,25 +76,28 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_novo_pedido_valido_seu_total_deve_ser_50()
         {
+            //setimo teste, testando o total do pedido
             var order = new Order(_customer, 10, _discount);
             order.AddItem(_product, 5);
-            Assert.AreEqual(order.Total(), 50);
+            Assert.AreEqual(order.Total(), 50); //resultado tem que dá 50
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_desconto_expirado_o_valor_do_pedido_deve_ser_60()
         {
+            //oitavo teste, testando desconto expirado
             var expiredDiscount = new Discount(10, DateTime.Now.AddDays(-5));
             var order = new Order(_customer, 10, expiredDiscount);
             order.AddItem(_product, 5);
-            Assert.AreEqual(order.Total(), 60);
+            Assert.AreEqual(order.Total(), 60); //valor do pedido deve ser 60
         }
 
         [TestMethod]
         [TestCategory("Domain")]
         public void Dado_um_desconto_invalido_o_valor_do_pedido_deve_ser_60()
         {
+            //nono teste, desconto invalido. É preciso ser testado..
             var order = new Order(_customer, 10, null);
             order.AddItem(_product, 5);
             Assert.AreEqual(order.Total(), 60);
@@ -99,6 +107,7 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_desconto_de_10_o_valor_do_pedido_deve_ser_50()
         {
+            //Teste de desconto valido
             var order = new Order(_customer, 10, _discount);
             order.AddItem(_product, 5);
             Assert.AreEqual(order.Total(), 50);
@@ -108,6 +117,7 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_uma_taxa_de_entrega_de_10_o_valor_do_pedido_deve_ser_60()
         {
+            //Teste de taxa de entrega
             var order = new Order(_customer, 10, _discount);
             order.AddItem(_product, 6);
             Assert.AreEqual(order.Total(), 60);
@@ -117,6 +127,7 @@ namespace Store.Tests.Domain
         [TestCategory("Domain")]
         public void Dado_um_pedido_sem_cliente_o_mesmo_deve_ser_invalido()
         {
+            //Tese pedido sem cliente
             var order = new Order(null, 10, _discount);
             Assert.AreEqual(order.Valid, false);
         }
